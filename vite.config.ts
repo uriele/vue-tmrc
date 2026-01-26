b8e05ce44fab4d3f629616aa8bb40f6cd2441142
Marco <m1menari@eng.ucsd.edu>
2026-01-26 23:39:51 +0100

    update workflow to bun

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,15 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/tmrc/' : '/',
+  plugins: [vue(), vueDevTools()],
   preview: {
     allowedHosts: ['tmrc.menalab.site'],
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+}))
