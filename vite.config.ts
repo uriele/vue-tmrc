34e454416755323a9e47ecfbcdca96bcfb427402
Marco <m1menari@eng.ucsd.edu>
2026-01-30 20:07:49 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vue-tmrc/',
+  base: '/',
   plugins: [vue(), vueDevTools()],
   preview: {
     allowedHosts: ['tmrc.menalab.site'],
