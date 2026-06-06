646ff70c63d1018d3c2591b9aed4f6ac3af65529
Marco <m1menari@eng.ucsd.edu>
2026-06-06 11:07:32 +0200

    remove ToTemporalTimezone

--- src/stores/__tests__/Deadlines.spec.ts
+++ src/stores/__tests__/Deadlines.spec.ts
@@ -1,40 +1,9 @@
-import { describe, expect, it } from "vitest";
+//import { describe, expect, it } from "vitest";
 //import { Deadline } from "../Deadline";
-import { Temporal } from "@js-temporal/polyfill";
-import { toTemporalZonedDateTime } from "../Deadline";
-const dateAsString = "2024-12-31";
-const temporalPlainDateTime = Temporal.PlainDateTime.from("2024-12-31T00:00:00");
-const temporalPlainDate = Temporal.PlainDate.from("2024-12-31");
-const temporalZonedDateTime = Temporal.ZonedDateTime.from("2024-12-31T00:00:00[Europe/Rome]");
-const timezone = "Europe/Rome";
-
-describe("toTemporalZonedDateTime Function", () => {
-  it("should convert a string date to Temporal.ZonedDateTime", () => {
-    const result = toTemporalZonedDateTime(dateAsString, timezone);
-    expect(result.toPlainDateTime().toString()).toBe("2024-12-31T00:00:00");
-    expect(result.timeZoneId).toBe("Europe/Rome");
-  });
-
-  it("should convert a Temporal.ZonedDateTime to itself", () => {
-    const result = toTemporalZonedDateTime(temporalZonedDateTime, timezone);
-    expect(result.toPlainDateTime().toString()).toBe("2024-12-31T00:00:00");
-    expect(result.timeZoneId).toBe("Europe/Rome");
-  });
-
-  it("should convert a Temporal.PlainDateTime to Temporal.ZonedDateTime", () => {
-    const result = toTemporalZonedDateTime(temporalPlainDateTime, timezone);
-    expect(result.toPlainDateTime().toString()).toBe("2024-12-31T00:00:00");
-    expect(result.timeZoneId).toBe("Europe/Rome");
-  });
-
-  it("should convert a Temporal.PlainDate to Temporal.ZonedDateTime", () => {
-    const result = toTemporalZonedDateTime(temporalPlainDate, timezone);
-    expect(result.toPlainDateTime().toString()).toBe("2024-12-31T00:00:00");
-    expect(result.timeZoneId).toBe("Europe/Rome");
-  });
-
-  it("should throw an error for invalid date formats", () => {
-    expect(() => toTemporalZonedDateTime("invalid-date", timezone)).toThrow();
-  });
-});
-
+//import { Temporal } from "@js-temporal/polyfill";
+//import { toTemporalZonedDateTime } from "../Deadline";
+//const dateAsString = "2024-12-31";
+//const temporalPlainDateTime = Temporal.PlainDateTime.from("2024-12-31T00:00:00");
+//const temporalPlainDate = Temporal.PlainDate.from("2024-12-31");
+//const temporalZonedDateTime = Temporal.ZonedDateTime.from("2024-12-31T00:00:00[Europe/Rome]");
+//const timezone = "Europe/Rome";
