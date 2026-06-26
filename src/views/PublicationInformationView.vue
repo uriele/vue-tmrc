edd7969cfa21fe30dc037d556696a2b55c2fb10e
Marco <m1menari@eng.ucsd.edu>
2026-06-26 03:20:29 +0200

    Update PublicationInformationView.vue

--- src/views/PublicationInformationView.vue
+++ src/views/PublicationInformationView.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import PageTitle from '@/components/PageTitle.vue'
-import { INSTRUCTIONSLINKS } from '@/router/routerConstants';
 import type { DownloadLink } from '@/stores/DownloadLinks';
 import DownloadButton from '@/components/DownloadButton.vue';
 interface Props{
@@ -8,11 +7,7 @@ interface Props{
   downloads?: DownloadLink[]
 }
 
-const props=withDefaults(defineProps<Props>(),
-{link: ()=> new URL('https://ieee.atyponrex.com/journal/magconf-ieee'),
- downloads: ()=> INSTRUCTIONSLINKS
-}
-)
+defineProps<Props>()
 </script>
 
 <template>
@@ -23,12 +18,12 @@ const props=withDefaults(defineProps<Props>(),
     </p>
     <p class="lead">
       Full manuscripts can be published in IEEE Transactions on Magnetics (subject to peer review).
-       Papers must be submitted before July 26th through the <a :href="props.link.href" target="_blank" rel="noopener noreferrer">IEEE AUTHOR PORTAL</a> website. The suggested lengths for contributed (posters) and invited papers are five pages and seven pages, respectively. The maximum paper length allowed by the journal is fourteen pages.
+       Papers must be submitted before July 26th through the <a :href="link?.href" target="_blank" rel="noopener noreferrer">IEEE AUTHOR PORTAL</a> website. The suggested lengths for contributed (posters) and invited papers are five pages and seven pages, respectively. The maximum paper length allowed by the journal is fourteen pages.
     </p>
     <p class="lead"> Instructions to submit your manuscript to the conference can be found here: </p>
 
-    <div class="d-flex justify-content-center align-items-center gap-4 p-2 lead">
-      <DownloadButton v-for="download in props.downloads" :key="download.filename" :downloads="download" />
+    <div class="d-flex justify-content-center align-items-center gap-4 p-2">
+      <DownloadButton v-for="download in downloads" :key="download.filename" :downloads="download" />
     </div>
   </section>
 </template>
